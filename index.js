e91593346f22d10aba3c40fba14ca216d7a82ba4
Nataraj <121968842+Nataraj1994@users.noreply.github.com>
2023-08-17 15:42:04 +0530

    Update index.js
    js updated

--- index.js
+++ index.js
@@ -1,163 +1,183 @@
+var tabbuttons = document.querySelectorAll(".tabcontainer .buttoncontainer button");
+var tabpanels = document.querySelectorAll(".tabcontainer .tabpanel");
+console.log(tabbuttons);
+console.log(tabpanels);
+
+
+function showpanel(panelIndex,Colorcode){
+    tabbuttons.forEach(function (node){
+        node.style.backgroundColor = "";
+        node.style.color = "";
+    });
+    tabbuttons[panelIndex].style.backgroundColor = Colorcode;
+    tabbuttons[panelIndex].style.color="white";
+    tabpanels.forEach (function (node){
+                                       node.style.display="none";
+                                      });
+    tabpanels[panelIndex].style.display="block";
+    tabpanels[panelIndex].style.backgroundColor=Colorcode;
+console.log(panelIndex);
+if(panelIndex==0){
+  materials.style.display="inline-block";
+  b11.style.display="inline-block";
+  a11.placeholder = "Enter Pickup point";
+}
+if(panelIndex==1){
+  materials.style.display="none";
+  b11.style.display="inline-block";
+  a11.placeholder = "Enter Pickup point";
+  
+}
+if(panelIndex==2){
+  materials.style.display="none";
+  b11.style.display="none";
+  a11.placeholder = "Enter Place Of work";
+}
+    
+}
+showpanel(0,'#f44336');
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 const buttons = document.querySelectorAll(".select");
+
 buttons.forEach(button => {
-    button.addEventListener("click", () => {
-      // Remove "selected" class from all buttons
-      buttons.forEach(btn => {
-        btn.classList.remove("selected");
-      });
-      
-        // Add "selected" class to the clicked button
-        button.classList.add("selected");
-      });
+   button.addEventListener("click", () => {
+    // Remove "selected" class from all buttons
+    buttons.forEach(btn => {
+      btn.classList.remove("selected");
     });
 
-    const btn=document.getElementById("driver");
-    btn.addEventListener("click",function(){
-    drop.style.display = "block";
-    });
-
-
-   const close=document.getElementById("close");
-   close.addEventListener("click",function(){
-    drop.style.display = "none";
-   })
-   function pass(selectedIndex) {
-    const buttons = document.querySelectorAll('.cargoandpass');
-  
-    buttons.forEach((button, index) => {
-        if (index === selectedIndex -1) {
-            button.classList.add('selected');
-        } else {
-            button.classList.remove('selected');
-        }
-    });
-    if (selectedIndex === 1) {
-      // Do something for Button 1
-      console.log("Button 1 clicked!");
-      bike.style.display = "none";
-      Auto.style.display = "none";
-      car.style.display = "none";
-      Traveller.style.display = "none";
-      Tiller.style.display = "none";
-      hitachi.style.display = "none";
-      jcb.style.display = "none";
-      auto.style.display = "inline-block";
-      tempo1.style.display = "inline-block";
-      tempo2.style.display = "inline-block";
-      tempo3.style.display = "inline-block";
-      tempo4.style.display = "inline-block";
-      Tipper.style.display = "inline-block";
-      Tractor.style.display = "inline-block";
-      material.style.display = "block";
-      agriplace.style.display = "none";
-      droping.style.display = "inline-block";
-      pickup.style.display = "inline-block";
-      to.style.display = "inline-block";
-      km.style.display = "block";
-      to1.style.display = "none";
-      
-
-
+    // Add "selected" class to the clicked button
+    button.classList.add("selected");
     
   
+    // Get the value associated with the clicked button
+    const selectedValue = parseInt(button.getAttribute("data-value"));
+   
+    // Define variables for different prices
+    const prices = {
+      1: 8,
+      2: 10,
+      3: 13,
+      4: 17,
+      5: 20,
+      6: 30,
+      8: 20,
+      9: 7,
+      10: 10,
+      11: 12,
+      12: 18,
+    };
   
-  
-  } else if (selectedIndex === 2) {
-      // Do something for Button 2
-      console.log("Button 2 clicked!");
-      auto.style.display = "none";
-      tempo1.style.display = "none";
-      tempo2.style.display = "none";
-      tempo3.style.display = "none";
-      tempo4.style.display = "none";
-      Tipper.style.display = "none";
-      Tractor.style.display = "none";
-      material.style.display = "none";
-      Tiller.style.display = "none";
-      hitachi.style.display = "none";
-      jcb.style.display = "none";
-      bike.style.display = "inline-block";
-      Auto.style.display = "inline-block";
-      car.style.display = "inline-block";
-      droping.style.display = "inline-block";
-      pickup.style.display = "inline-block";
-      agriplace.style.display = "none";
-      Traveller.style.display = "inline-block";
-      to.style.display = "inline-block";
-    km.style.display = "block";
-    to1.style.display = "none";
-
-
-  
-  
-  
-  } else if (selectedIndex === 3) {
-      // Do something for Button 3
-      console.log("Button 3 clicked!");
-    // cargo.style.background = rgb(233, 233, 235);
-
-    Tiller.style.display = "inline-block";
-    hitachi.style.display = "inline-block";
-    jcb.style.display = "inline-block";
-    auto.style.display = "none";
-    tempo1.style.display = "none";
-    tempo2.style.display = "none";
-    tempo3.style.display = "none";
-    tempo4.style.display = "none";
-    Tipper.style.display = "none";
-    Tractor.style.display = "none";
-    material.style.display = "none";
-    bike.style.display = "none";
-    Auto.style.display = "none";
-    car.style.display = "none";
-    pickup.style.display = "none";
-    agriplace.style.display = "block";
-    droping.style.display = "none";
-    Traveller.style.display = "none";
-    km.style.display = "none";
-    to.style.display = "none";
-    to1.style.display = "block";
-
+    // Use the selected value to get the price from the 'prices' object
+    const price = prices[selectedValue];
+   
     
-  
-  
-  
-  }
- 
-   }
-   window.addEventListener('DOMContentLoaded',function(){
-    console.log('clicked');
-    cargo.style.background = "orange";
-    cargo.style.color = "black";
+    // var selectedValueElement =prices[selectedValue]; 
+    var km1 = document.getElementById("km1").value;
+    console.log(km1);
+    console.log(prices[selectedValue]);
+    function myFunction12(){
+    
+      var selectedValueElement =prices[selectedValue]; 
+      var km1 = document.getElementById("km1").value;
 
+     let x1=(selectedValueElement*2) * km1;
+      document.getElementById("priceleft").value=x1;
+      console.log(x1);
+      return x1;
+    };
+    myFunction12();                                     //total amount have to pay (customer side)
+   
+    // Use the selected value as needed
+    var text = document.getElementById("kmprice");
+    text.textContent = "Per KM Price : " + price + " RS";
+    
+  });
+});
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    bike.style.display = "none";
-    Auto.style.display = "none";
-    car.style.display = "none";
-    Traveller.style.display = "none";
-    Tiller.style.display = "none";
-    hitachi.style.display = "none";
-    jcb.style.display = "none";
-    auto.style.display = "inline-block";
-    tempo1.style.display = "inline-block";
-    tempo2.style.display = "inline-block";
-    tempo3.style.display = "inline-block";
-    tempo4.style.display = "inline-block";
-    Tipper.style.display = "inline-block";
-    Tractor.style.display = "inline-block";
-    material.style.display = "block";
-    agriplace.style.display = "none";
-    droping.style.display = "inline-block";
-    pickup.style.display = "inline-block";
-    to.style.display = "inline-block";
-    to1.style.display = "none";
+  var driver=document.getElementById("driver");
+  driver.addEventListener('click',()=>{
+    drop.style.display="block";
 
   });
+  var close=document.getElementById("close");
+  close.addEventListener('click',()=>{
+    drop.style.display="none";
+
+  });
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   function icon1(){
     mainul.style.display ="block";
   }
   function homepage(){
     mainul.style.display ="none";
-  ;
   }
+
+ function registered(){
+  var drivername = document.getElementById("drivername").value;
+  var drivernumber = document.getElementById("drivernum").value;
+  let driverliecence = document.getElementById("driverliecence").value;
+  let drivervehiclenum = document.getElementById("drivervehiclenum").value;
+  let vehiclehave = document.getElementById("vehiclehave").value;
+  
+  localStorage.setItem("localstoragedrivername",drivername);
+  localStorage.setItem("localstoragedrivernum",drivernumber);
+  localStorage.setItem("localstoragedriverliecence",driverliecence);
+  localStorage.setItem("localstoragedrivervehiclenum",drivervehiclenum);
+  localStorage.setItem("localstoragedrivervehiclehave",vehiclehave);
+  document.getElementById("submit").addEventListener('click',function(){
+  document.getElementById("form").reset();
+  // Form.style.display="none";
+  // document.getElementById("Successfully").innerHTML="Successfully registered";
+ });
+};
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function orderconfirm(){
+ var p=document.getElementById("a11").value;
+ var q=document.getElementById("b11").value;
+ var s=document.getElementById("username").value;
+ var t=document.getElementById("usermobile").value;
+ if(!p||!q||!s||!t){
+  console.log(p,q,s,t);
+
+  alert("Please enter values in all fields.");
+}else{
+  document.getElementById("customer").style.display="block";
+  console.log(p,q,s,t);
+
+
+ 
+ var a=document.getElementById("a11").value;
+ document.getElementById("c_pickup").innerHTML=a;
+ var b=document.getElementById("b11").value;
+ document.getElementById("c_Drop").innerHTML=b;
+ var c=localStorage.getItem("localstoragedrivername");
+ document.getElementById("c_Dname").innerHTML=c;
+ var d=localStorage.getItem("localstoragedrivernum");
+ document.getElementById("c_Dnumber").innerHTML=d;
+ var e=localStorage.getItem("localstoragedrivervehiclenum");
+ document.getElementById("c_Vnum").innerHTML=e;
+ var f=localStorage.getItem("localstoragedrivervehiclehave");
+ document.getElementById("c_vehicle").innerHTML=f;
+ var g=document.getElementById("priceleft").value;
+document.getElementById("c_Price").innerText= g;
+var h=document.getElementById("tkm").value;
+console.log('result is ' + h);
+document.getElementById("c_km").innerHTML=h;
+ 
+var closebox=document.getElementById("c_close");
+closebox.addEventListener('click',()=>{
+  customer.style.display="none";
+});
+};
+};
+  
+ 
+
+  
+
+ 
+
+
 
